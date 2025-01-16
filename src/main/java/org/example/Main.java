8e2b4d2ae838170e5e3836b27e1f4349082e2c29
Minsol Kim <yhe071624@gmail.com>
2025-01-16 15:15:24 +0900

    0116 #3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.menu.Menu;
 import org.example.store.BasicRes;
 import org.example.store.BusanRes;
 import org.example.store.DaeguRes;
@@ -12,21 +13,24 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //abstract 붙음 : 추상 클래스 -> 객체 생성 불가, 물려주거나 타입으로는 유용 Menu[] m = new Menu();불가
+        //Menu m= new Coffee();는 가능
+        Menu[]m =new Menu[10];
 
-        HashMap<String,BasicRes> resHashMap = new HashMap<>();
-
-        resHashMap.put("서울",new BasicRes());
-        resHashMap.put("부산",new BusanRes());
-        resHashMap.put("대구",new DaeguRes());
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("도시를 입력하세요");
-        String city = scanner.nextLine();
-
-
-        BasicRes target = resHashMap.get(city);
-
-        target.makeZazang();
+//        HashMap<String,BasicRes> resHashMap = new HashMap<>();
+//
+//        resHashMap.put("서울",new BasicRes());
+//        resHashMap.put("부산",new BusanRes());
+//        resHashMap.put("대구",new DaeguRes());
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("도시를 입력하세요");
+//        String city = scanner.nextLine();
+//
+//
+//        BasicRes target = resHashMap.get(city);
+//
+//        target.makeZazang();
 
 
 
